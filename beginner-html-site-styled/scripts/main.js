f8c23610d28944f691e0f3c76fccbe7e2144541f
yuanli cheng <141103822+xiaoli0809@users.noreply.github.com>
2023-08-04 12:33:28 +0800

    Add files via upload

---
+++ beginner-html-site-styled/scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+//取名字函数
+function setUserName() {
+    let myName = prompt("请输入你的名字。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    }
+  }
+  
+//调用已有的名字
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+  }
+//点击调用
+  myButton.onclick = function () {
+    setUserName();
+  };
+  
